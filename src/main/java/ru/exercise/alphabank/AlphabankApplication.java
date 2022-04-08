7ee02c37663c1582df5b81e2c8e95363aa29ab91
kulich51 <59617932+kulich51@users.noreply.github.com>
2022-04-08 17:41:47 +0300

    Initial commit

---
+++ src/main/java/ru/exercise/alphabank/AlphabankApplication.java
@@ -0,0 +1,13 @@
+package ru.exercise.alphabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphabankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphabankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/exercise/alphabank/AlphabankApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.exercise.alphabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphabankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
